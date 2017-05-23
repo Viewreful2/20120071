856a3482b7a53a8ab4d0c90817f3f95fdfd84fbd
namhyun <joochan9421@naver.com>
2017-05-23 04:26:44 +0000

    "20120071"

---
+++ 20120071.py
@@ -0,0 +1,39 @@
+row = 5
+col = 5
+
+BINGO = []
+
+T = int(input())
+
+for t in range(0,T):
+
+    for i in range(0, row):
+        row_input = input().split()
+        row_input = [int(j) for j in row_input]
+        BINGO.append(row_input)
+    print(BINGO)
+
+    input_numbers = input(). split()
+    input_numbers = [int(j) for j in input_numbers]
+
+    count_of_input = 0
+    for number in input_numbers:
+
+        coumt_of_input = count_of_input + 1
+    
+    
+        if rule1(number) == true
+        print(count_of_input)
+        exit
+
+        if rule2(number) == true
+        print(count_of_input)
+        exit
+
+        if rule3(number) == true
+        print(count_of_input)
+        exit
+
+        if rule4(number) == true
+        print(count_of_input)
+        exit
